0523455919511f12a47f99e4c1fbd9eed964a06f
SohamBhandary <bhandarysoham27@gmail.com>
2026-08-13 16:39:20 +0530

    Setup Location Serivce with updateDriveLocations, getNearByDriver and remove driver apis and setup redis config

---
+++ LocationService/src/main/java/com/RideShare/LocationService/Controllers/LocationController.java
@@ -0,0 +1,45 @@
+package com.RideShare.LocationService.Controllers;
+
+import com.RideShare.LocationService.DTOs.DriverLocationRequest;
+import com.RideShare.LocationService.DTOs.NearByDriverResponse;
+import com.RideShare.LocationService.Services.LocationSerivice;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/locations")
+@Slf4j
+@RequiredArgsConstructor
+public class LocationController {
+    private final LocationSerivice locationService;
+
+    //driver phone calls this every 3 second
+    @PostMapping("/drivers/update")
+    public ResponseEntity<String> updateDriverLocation(
+            @RequestBody DriverLocationRequest driverLocationRequest){
+        locationService.updateDriverLocation(driverLocationRequest);
+        return ResponseEntity.ok("Driver Location updated");
+    }
+
+
+    //Matching service calls this when ride is requested
+    @GetMapping("/drivers/nearby")
+    public ResponseEntity<List<NearByDriverResponse>> getNearByDrivers(
+            @RequestParam double latitude,
+            @RequestParam double longitude,
+            @RequestParam (defaultValue = "5.0") double radius) {
+
+        return ResponseEntity.ok(locationService.findNearbyDrivers(latitude, longitude, radius));
+    }
+
+    // called when driver goes offline
+    @DeleteMapping("/drivers/{driverID}")
+    public ResponseEntity<String> removeDriver(@PathVariable String driverID){
+        locationService.removeDriver(driverID);
+        return ResponseEntity.ok("Driver removed successfully");
+    }
+}
